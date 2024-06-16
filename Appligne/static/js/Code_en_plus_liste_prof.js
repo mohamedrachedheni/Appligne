7cb812df3acec5c92ba9129fc0b6e5a3059f337c
mohamedrachedheni <prosib25@gmail.com>
2024-06-16 14:26:50 +0100

    ajout evennement touchstart au JS liste prof 240616

--- Appligne/static/js/Code_en_plus_liste_prof.js
+++ Appligne/static/js/Code_en_plus_liste_prof.js
@@ -1,6 +1,6 @@
 /*********************** http://localhost:8000/liste_prof  début **************************** */
 
-// Fonction pour modifier le texte en fonction de la réduction d'impôts
+// Fonction pour modifier le texte et les prix en fonction de la réduction d'impôts
 function toggleText() {
     const priceText = document.getElementById("priceText");
     const prixParHeureElements = document.querySelectorAll(".prix_par_heure");
@@ -22,7 +22,7 @@ function toggleText() {
 
 /*********************** http://localhost:8000/liste_prof  fin **************************** */
 
-// Fonction pour mettre à jour la valeur de l'input lorsque l'utilisateur sélectionne un élément dans la liste déroulante
+// Fonction pour mettre à jour la valeur de l'input lorsqu'un élément dans la liste déroulante est sélectionné
 function updateInputValue(event) {
     const inputId = event.target.closest('.dropdown').querySelector('input[type="text"]').id;
     const selectedValue = event.target.getAttribute('data-value');
@@ -34,26 +34,36 @@ function updateInputValue(event) {
 // Gestion des événements
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Afficher le menu déroulant lorsqu'un input est cliqué
+    // Fonction pour afficher le menu déroulant
+    function showDropdown(event) {
+        const dropdown = event.currentTarget.parentElement.querySelector('.dropdown-menu');
+        dropdown.style.display = 'block';
+        event.stopPropagation();
+    }
+
+    // Sélectionne tous les inputs de type "text"
     const inputs = document.querySelectorAll('input[type="text"]');
     inputs.forEach(input => {
-        input.addEventListener('click', function(event) {
-            const dropdown = input.parentElement.querySelector('.dropdown-menu');
-            dropdown.style.display = 'block';
-            event.stopPropagation();
-        });
+        // Ajoute un événement 'click' et 'touchstart' à chaque input
+        input.addEventListener('click', showDropdown);
+        input.addEventListener('touchstart', showDropdown);
     });
 
-    // Mettre à jour l'input lorsqu'un élément de la liste déroulante est cliqué
+    // Sélectionne tous les éléments de la liste déroulante
     const dropdownItems = document.querySelectorAll('ul.dropdown-menu a.dropdown-item');
     dropdownItems.forEach(item => {
+        // Ajoute un événement 'click' et 'touchstart' à chaque élément de la liste déroulante
         item.addEventListener('click', function(event) {
+            updateInputValue(event);
+            event.preventDefault();
+        });
+        item.addEventListener('touchstart', function(event) {
             updateInputValue(event);
             event.preventDefault();
         });
     });
 
-    // Masquer le menu déroulant lorsqu'on clique en dehors
+    // Ajoute un événement 'click' au document pour masquer les menus déroulants lorsque l'utilisateur clique en dehors
     document.addEventListener('click', function(event) {
         const dropdowns = document.querySelectorAll('.dropdown-menu');
         dropdowns.forEach(dropdown => {
@@ -63,7 +73,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    // Soumettre le formulaire lorsqu'un élément de la liste déroulante est cliqué
+    // Sélectionne le menu déroulant de la région
     const dropdownRegion = document.getElementById("dropdownMenu_region_id");
     dropdownRegion.addEventListener("click", function(event) {
         event.preventDefault(); 
@@ -72,7 +82,7 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById("liste_prof_id").submit(); 
     });
 
-    // Actualiser le formulaire lorsqu'une case est cochée
+    // Sélectionne toutes les cases à cocher avec la classe 'form-check-input'
     const checkboxes = document.querySelectorAll('.form-check-input');
     checkboxes.forEach(checkbox => {
         checkbox.addEventListener('change', function(event) {
