802e3b33bfeae36eb74f36615557fe68dc01d532
mohamedrachedheni <prosib25@gmail.com>
2026-01-22 02:38:08 +0100

    payment_intent.canceled

--- Appligne/payment/views.py
+++ Appligne/payment/views.py
@@ -1713,7 +1713,7 @@ def stripe_webhook(request):
             #'payment_intent.created': handle_payment_intent_created, # mise à jour Invoce.stripe_payment_intent_id et création ou mise à jour d'un enregistrement dans Payment (achevé)
             'checkout.session.expired': handle_checkout_session_expired, # mise à jour Invoice.status=CANCELED et Demande_paiement.EN_ATTENTE (achevé)
             #'checkout.session.completed': handle_checkout_session_completed, # à suivre
-            'payment_intent.canceled': handle_payment_intent_canceled, # Cet événement signifie que le PaymentIntent a été annulé avant tout débit réel. Exemple : l’élève abandonne le paiement avant de valider, ou le paiement expire.
+            #'payment_intent.canceled': handle_payment_intent_canceled, # Cet événement signifie que le PaymentIntent a été annulé avant tout débit réel. Exemple : l’élève abandonne le paiement avant de valider, ou le paiement expire.
             'payment_intent.payment_failed': handle_payment_intent_failed, # Ce cas se produit lorsque le paiement a été tenté mais refusé par la banque (fonds insuffisants, carte expirée, etc.).
             #'payment_intent.succeeded': handle_payment_intent_succeeded, # Mettre à jour le statut 
             # ==========================================================
